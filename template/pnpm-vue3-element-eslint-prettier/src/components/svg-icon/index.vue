ca9548c73e0d0a448e0b509a5f8f41d9b71240ce
wuchi <wuchi57@outlook.com>
2024-03-11 17:36:48 +0800

    reset

---
+++ template/pnpm-vue3-element-eslint-prettier/src/components/svg-icon/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <svg class="svg-icon" aria-hidden="true">
+    <use :href="symbolId" :fill="color" />
+  </svg>
+</template>
+
+<script setup>
+const props = defineProps({
+  dir: {
+    type: String,
+    default: '',
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+  },
+})
+const symbolId = computed(() => `#svg_${props.dir}_${props.name}`)
+</script>
+
+<style lang="sass" scoped>
+.svg-icon
+  width: 1em
+  height: 1em
+  fill: currentColor
+  vertical-align: text-bottom
+</style>
